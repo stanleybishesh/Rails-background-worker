dfa433f9526baa62068f5f17de5848444d110dde
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-08-09 17:51:04 +0545

    Mailer scheduled in sidekiq

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
     default from: 'bishesh.shrestha@fleetpanda.com'
 
-    def hello_email
-        @user = params[:user]
+    def hello_email(user)
+        @user = user
         @url = "https://stanleybishesh.github.io/Fleet-Project/"
-        mail(to: @user.email, subject: "Please feel free to user my test website")
+        mail(to: user.email, subject: "Please feel free to user my test website")
     end
 end
